5135834e20205ed265bb4c3d4423a8ee9dc7a05b
user <user@localhost.localdomain>
2019-03-14 15:09:43 +0700

    Unittest polls app#1

--- test_14/polls/tests.py
+++ test_14/polls/tests.py
@@ -3,6 +3,14 @@ from django.urls import resolve
 from polls.views import home_page
 
 class HoemPageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
+    def test_root_url_resolves_to_index_view(self):
+        found = resolve("/polls/")
+        self.assertEqual(found.func, index)
+
+class DetailPageTest(TestCase):
+    def test_root_url_resolves_to_detail_view(self):
+        found = resolve("/polls/<int:question_id>")
+        self.assertEqual(found.func, detail)
+
+    def test_model_detail(self):
+        pass
