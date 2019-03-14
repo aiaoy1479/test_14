bd24d69c6e1f519a988d67fe9ceb06bf84f930c5
user <user@localhost.localdomain>
2019-03-14 15:03:36 +0700

    Create app name polls and write Functional tests.py

--- test_14/functional_tests.py
+++ test_14/functional_tests.py
@@ -1,4 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class QuestionTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_question_poll(self):
+        browser = webdriver.Firefox()
+        #ผู้ใช้เปิดหน้าเว็บขึ้นมา
+        browser.get('http://localhost:8000/polls/1/')
+        #เห็นชื่อ title
+        self.assertIn('xxxx', self.browser.title)
+        #เห็นคำถาม
+        question_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('คำถาม', question_text)
+        #เห็นตัวเลือก
+        choice_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('ตัวเลือก', choice_text)
+
+
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
